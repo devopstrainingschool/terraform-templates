c53e684c16c5e61a6a97b4aeaf39bfa77bf36dbb
devopstrainingschool <107158398+devopstrainingschool@users.noreply.github.com>
2023-05-07 11:51:02 -0500

    Update remote-exec.provisioner.tf

--- remote-exec.provisioner.tf
+++ remote-exec.provisioner.tf
@@ -5,3 +5,12 @@ provisioner "remote-exec"  {
     
       ]
    }
+connection {
+    type         = "ssh"
+    host         = self.public_ip
+    user         = "ec2-user" # change user"ec2-user" for amz
+    timeout = "2m"
+    agent = false
+    private_key  = tls_private_key.anael2.private_key_pem #"${file("anael.pem")}" 
+   }
+  
